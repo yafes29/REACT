2eb15ab50372cd6256688316cd22946918044390
mustafa <yafess229@gmail.com>
2022-06-24 18:09:51 +0300

    update

--- rc03_sass/src/App.js
+++ rc03_sass/src/App.js
@@ -1,15 +1,17 @@
 
-import Card from "./components/Card";
+import Card from "./components/Card.jsx";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
-import "./scss/App.scss"
+import "./scss/App.scss";
+import data from "./data";
 
 function App() {
   return (
+    
     <>
-      
+     
       <Header />
-      <Card />
+      <Card data={data} />
       <Footer />
     </>
   );
