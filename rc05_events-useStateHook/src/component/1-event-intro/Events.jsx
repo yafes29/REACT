dfae3be45d184018e07cdcc3c1620074f6e624c0
mustafa <yafess229@gmail.com>
2022-06-29 15:17:02 +0300

    info

--- rc05_events-useStateHook/src/component/1-event-intro/Events.jsx
+++ rc05_events-useStateHook/src/component/1-event-intro/Events.jsx
@@ -12,6 +12,8 @@
 
 const Events = () => {
 
+  let info = "EVENTS"
+
   const handleClick = ()=>{
     alert("Buton Clicked")
   }
@@ -20,10 +22,14 @@ const Events = () => {
     alert(text);
   }
   const handleChange =()=>{
+    info = "HOOKS";
+    console.log(info);
 
   }
   return (
     <div className="container text-center mt-4">
+
+      <div className="display-4 text-danger m-4">{info}</div>
       <button onClick={handleClick} className="btn btn-warning">Click</button>
        
        {/* Eger bir event fonksiyonunun paremetresi olmasi gerkiyorsa
@@ -39,3 +45,18 @@ const Events = () => {
 }
 
 export default Events
+
+//! info console'da guncellendigini ancak DOM'da guncellenmedigini gorduk.
+//* Bunun sebebi REACT'in aksi belirtilmedigi surece elementleri
+//* static olarak kabul etmesinden kaynaklanir.
+
+//* React bunu DOM islemlerini minimize etmek icin yapmaktadir.
+//* REACT'a hangi elementleri interaktif oldugu belirtilmelidir.
+//! React'a elementlerin interaktif oldugunu belirtmek icin state'ler kullanilir.
+//! State, elementlerin degisiklik durumlarini tutan nesnelerdir.
+//? ReactJs'de state'leri kullanmak icin  2 ayri Component yapisi bulunmaktadir.
+//? 1. Statefull Classes (Class Components).
+//? 2. Hooks (Functional Components).
+
+//?Biz su ana kadar uygulamalarimizda Fonksiyonel Component'leri kullandik.
+//? Yaygin kullanim Fonksiyonel Component'lerdir.
